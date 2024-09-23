611283875be0bab8f3c87b4041054a698d1cf463
“yalongwang2” <yalongwang2@creditease.cn>
2024-09-23 16:45:25 +0800

    基础完善

--- src/App.js
+++ src/App.js
@@ -1,25 +1,50 @@
-import logo from './logo.svg';
-import './App.css';
+import './App.less';
+import React  from 'react'
+import { BrowserRouter, useHistory   } from 'react-router-dom'
+import { renderRoutes } from 'react-router-config'
+import { KeepaliveRouterSwitch } from 'react-keepalive-router'
+import JSX from './page/jsx'
+const menusList = [
+  {
+    name: '2_认识Jsx',
+    path: '/jsx',
+    component:JSX
+  },
+]
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+function Meuns(){
+  const history = useHistory()
+   return <div className="theStyle" >
+   {menusList.map(item=><span className="routerLink"
+       key={item.path}
+       onClick={()=> {  history.push(item.path) }}
+                        >{item.name}</span>)}
+  </div>
 }
 
-export default App;
+export default  class Index extends React.Component{
+  state={
+    Number:1
+  }
+  node = null
+  componentDidMount(){
+    // console.log(this.node)
+    // console.log(this)
+    console.log(this)
+  }
+
+  render(){
+    return <div ref={(node) => this.node = node} >
+      <div >
+          <BrowserRouter>
+            <Meuns/>
+            <div style={{ paddingTop:'50px' }} >
+              <KeepaliveRouterSwitch withoutRoute >
+                  {renderRoutes(menusList)}
+              </KeepaliveRouterSwitch>
+            </div>
+          </BrowserRouter>
+      </div>
+  </div>
+  }
+}
